3251341d45acdfa0bdfeaf396d51635902a9939e
Peter JJ MacDonald <peter@jjmacdonald.com>
2015-02-13 06:03:16 -0500

    Reduced randomness to make cloud code run faster

--- cloud/main.js
+++ cloud/main.js
@@ -7,7 +7,7 @@ var query = new Parse.Query("CardObject");
 //Here, the only selector I am looking for is that it hasn't had it's gender set.
 query.doesNotExist("gender");
 //The default size of the array is 100 at it's largest, we want to get 1000 to reduce the amount of repeats the user sees.
-query.limit(1000);
+query.limit(100);
 query.find({success: function(results) { 
 //We select a random result from the array.
 response.success(results[parseInt((Math.random()*results.length))].get("multiverseid"));
